098905afa8349031339f069b3d8a12cb92df7630
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-05-19 23:37:40 -0300

    Create Lorem ipsum generator in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import data from './data';
+import data from './components/data';
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
